bf52cd007b2e810ea0e6bf7542bc6689156cd250
tburkett2027 <tburkett2027@student.amsacs.org>
2026-05-29 13:14:28 -0400

    initial commit

---
+++ Rocket.py
@@ -0,0 +1,9 @@
+class Rocket:
+    def __init__(self):
+        self.position: tuple[float, float]
+        self.velocity: tuple[float, float]
+        self.angular_velocity: float
+        self.angle: float
+
+    def getVertices(self) -> tuple[tuple[float, float]]:
+        pass
