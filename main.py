5fad5b083bf34074d70ceac7efa199a1404ceba0
SourMnMs <tburkett525@gmail.com>
2026-06-02 01:18:13 -0400

    Update main.py
    so i was kinda stupid but im better now and input is so much easier than i thought it was

--- main.py
+++ main.py
@@ -11,12 +11,11 @@ clock = pygame.time.Clock()
 
 start_time: float = time.perf_counter()
 def gamePrint(inp: str) -> float:
-    print(f"{round(time.perf_counter() - start_time, 4)}s {inp}")
+    print(f"<{round(time.perf_counter() - start_time, 4)}s>\t{inp}")
 
 gamePrint("Starting game")
 
 deltaTime: float = 0.0 # fix later
-pressedKeys: set[str] = set()
 
 while True:
 
@@ -26,34 +25,25 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             raise SystemExit
+
+    # EDUARDO!!!
+    # to check a key, literally just call key[pygame.K_something]
+    # all key codes are here https://www.pygame.org/docs/ref/key.html
+    key = pygame.key.get_pressed()
+
+    if key[pygame.K_ESCAPE]:
+        gamePrint("<!> Quitting! <!>")
+        pygame.quit()
+        raise SystemExit
         
-        if event.type == pygame.KEYDOWN:
-            key = pygame.key.get_pressed()
 
-            if key[pygame.K_ESCAPE]:
-                gamePrint("<!> Quitting! <!>")
-                pygame.quit()
-                raise SystemExit
-            
-            if key[pygame.K_w]: pressedKeys.add('w')
-            if key[pygame.K_a]: pressedKeys.add('a')
-            if key[pygame.K_s]: pressedKeys.add('s')
-            if key[pygame.K_d]: pressedKeys.add('d')
-
-        if event.type == pygame.KEYUP:
-            key = pygame.key.get_pressed()
-            if key[pygame.K_w]: pressedKeys.discard('w')
-            if key[pygame.K_a]: pressedKeys.discard('a')
-            if key[pygame.K_s]: pressedKeys.discard('s')
-            if key[pygame.K_d]: pressedKeys.discard('d')
+    if key[pygame.K_w]: gamePrint("we're going up up up")
+    if key[pygame.K_a]: gamePrint("to the left to the left to the left to the left")
+    if key[pygame.K_s]: gamePrint("watchu know about rolling down in the deep")
+    if key[pygame.K_d]: gamePrint("to the right to the right to the right to the right")
 
 
     # ===== LOGIC GOES HERE =====
-    print(f"Pressed keys: {pressedKeys}")
-
-    # input logic demo
-    if 'w' in pressedKeys:
-        print("wow that's really cool")
 
 
     # ===== RENDERING GOES HERE =====
